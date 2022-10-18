905a7ccaed7a5c5ee22db2b315bad41274e436d0
Sinan Metin <sinanmetin53@gmail.com>
2022-10-18 14:42:27 +0200

    pretty reports deleted

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "pretty",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/library/stepdefinitions",
